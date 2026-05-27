ff60278ff5f442a68638c28574e0c2b47e6d6cba
Felipe Chaves <felipechavespsp@gmail.com>
2026-05-27 19:48:01 -0300

    Arrays internos

--- Arrays e slices/arrays.go
+++ Arrays e slices/arrays.go
@@ -13,4 +13,10 @@ func main() {
 
 	slice = append(slice, 6) // para adicionar um elemento a um slice, utilizamos a função append, passando o slice e o elemento que queremos adicionar
 	fmt.Println(slice)
+
+	// Arrays Internos
+	slice3 := make([] float32, 10, 15) // a função make é utilizada para criar um slice, passando o tipo de dado, o tamanho e a capacidade do slice. O tamanho é o número de elementos que o slice tem atualmente, e a capacidade é o número máximo de elementos que o slice pode ter antes de precisar ser redimensionado.
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) // a função len retorna o tamanho do slice
+	fmt.Println(cap(slice3)) // a função cap retorna a capacidade do slice
 }
