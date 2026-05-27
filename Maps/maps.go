9183b54e406a77758dc201b36a7f371b2120fa6d
Felipe Chaves <felipechavespsp@gmail.com>
2026-05-27 20:01:54 -0300

    Maps

---
+++ Maps/maps.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// map é uma coleção de pares chave-valor, onde cada chave é única e é associada a um valor.
+	usuario := map[string]string { 
+		"nome": "João",
+		"sobrenome": "Chaves",
+		
+	}
+
+	fmt.Println(usuario)
+}
