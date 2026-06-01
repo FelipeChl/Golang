5593da64b0e345026fb65fcbcddeb5ce46b8cd8d
Felipe Chaves <felipechavespsp@gmail.com>
2026-06-01 19:32:01 -0300

    Switch

---
+++ Switc/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func diaDaSemana (numero int) string {
+
+	switch numero {
+	case 1:
+		return "Domingo"
+	case 2:
+		return "Segunda-feira"
+	case 3:
+		return "Terça-feira"
+	case 4:
+		return "Quarta-feira"
+	case 5:
+		return "Quinta-feira"
+	case 6:
+		return "Sexta-feira"
+	case 7:
+		return "Sábado"
+	default:
+		return "Número inválido"
+	}
+
+}
+
+func main() {
+	// Estruturas de controle
+	// Switch
+
+	dia := diaDaSemana(3)
+
+	fmt.Println(dia)
+
+
+}
